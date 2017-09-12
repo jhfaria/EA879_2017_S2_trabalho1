af2d97ffb3e18ea07a059cea675f52caa54aec07
João Henrique Faria <jhfariaa@gmail.com>
2017-09-12 01:33:07 -0300

    Versão com a pasta OpenCV

---
+++ opencv/DisplayImage.cpp
@@ -0,0 +1,46 @@
+#include <cv.h>
+#include <highgui.h>
+#include <iostream>
+
+using namespace cv;
+
+double alpha; /**< Simple contrast control */
+int beta;  /**< Simple brightness control */
+
+int main( int argc, char** argv )
+{
+ /// Read image given by user
+ Mat image = imread( argv[1] );
+ Mat new_image = Mat::zeros( image.size(), image.type() );
+
+ /// Initialize values
+ std::cout<<" Basic Linear Transforms "<<std::endl;
+ std::cout<<"-------------------------"<<std::endl;
+ std::cout<<"* Enter the alpha value [1.0-3.0]: ";std::cin>>alpha;
+ std::cout<<"* Enter the beta value [0-100]: "; std::cin>>beta;
+
+ /// Do the operation new_image(i,j) = alpha*image(i,j) + beta
+ for( int y = 0; y < image.rows; y++ )
+    { for( int x = 0; x < image.cols; x++ )
+         { for( int c = 0; c < 3; c++ )
+              {
+      new_image.at<Vec3b>(y,x)[c] =
+         saturate_cast<uchar>( alpha*( image.at<Vec3b>(y,x)[c] ) + beta );
+             }
+    }
+    }
+
+ imwrite( "teste_brilho.jpg", new_image);
+
+ /// Create Windows
+ namedWindow("Original Image", CV_WINDOW_AUTOSIZE);
+ namedWindow("New Image", CV_WINDOW_AUTOSIZE);
+
+ /// Show stuff
+ imshow("Original Image", image);
+ imshow("New Image", new_image);
+
+ /// Wait until user press some key
+ waitKey();
+ return 0;
+}
